95f6f78c9d94a13675732a055d16c62bbe98e655
enoch232 <enoch232@gmail.com>
2016-06-15 13:26:32 -0400

    add posts index, and other methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+  before_action :find_params, only: [:new, :create, :destroy, :update, :edit]
+  def index
+  	@posts = Post.all
+  end
+  def new 
+  	@post = Post.new
+  end
+  def create
+  	@post = Post.new(post_params)
+  end 
+  def show
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  	@post.destroy
+  	redirect_to root_path
+  end
+  private
+  def post_params 
+  	params.require(:posts).permit(:title, :description, :price)
+  end
+  def find_params
+  	@post = Post.find(params[:id])
+  end
 end
