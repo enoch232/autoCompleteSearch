eb60c8f8f3ddfc5fdf90c3290b340ad06eeeb5a4
enoch232 <enoch232@gmail.com>
2016-06-15 13:17:58 -0400

    add search controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615170426) do
+ActiveRecord::Schema.define(version: 20160615171738) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20160615170426) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
