471e71f12184252b2d363169a7c9386fb7d0aac7
enoch232 <enoch232@gmail.com>
2016-06-15 23:52:09 -0400

    update the search suggestions

--- app/models/search_suggestion.rb
+++ app/models/search_suggestion.rb
@@ -1,2 +1,7 @@
 class SearchSuggestion < ApplicationRecord
+  attr_accessible :popularity, :term
+  def self.search_term(prefix)
+  	suggestions = where("term LIKE ?","#{prefix}_%")
+  	suggestions.order("popularity desc").limit(10).pluck(:term)
+  end
 end
